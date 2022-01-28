a72f27cc4d42d031056a437f588ed15020b9244e
Randhyll <randhallr@portobellosouth.com>
2022-01-28 01:18:07 -0300

    Promotion Sales Fravega

---
+++ src/PromotionSales.Api.WebUI/Startup.cs
@@ -0,0 +1,92 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.WebUI.Filters;
+using CleanArchitecture.WebUI.Services;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddApplication();
+
+        services.AddInfrastructure(Configuration);
+
+        services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+        services.AddHttpContextAccessor();
+
+        services.AddControllers();
+
+        services.AddRazorPages();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
+
+        services.AddHealthChecks()
+       .AddDbContextCheck<ApplicationDbContext>();
+
+        services.AddControllersWithViews(options =>
+            options.Filters.Add<ApiExceptionFilterAttribute>())
+                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+
+        services.AddRazorPages();
+
+        // Customise default API behaviour
+        services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHealthChecks("/health");
+        app.UseHttpsRedirection();
+        app.UseSwagger();
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core Order Shop API");
+            c.RoutePrefix = string.Empty;
+        });
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+       
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
+    }
+}
